47da544a3e4b8d62091faa04b5d96ae3f68cd1c6
Hayser8 <gar22076@uvg.edu.gt>
2025-02-10 18:54:39 -0600

    main para probar el nuevo shunting yard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from shunting_yard import shunting_yard
-from symbol import Symbol  
 
 if __name__ == "__main__":
     test_expressions = [
@@ -7,11 +6,18 @@ if __name__ == "__main__":
         "(a|b)*.c", 
         "a.b|c*",  
         "a?(b|c)+",  
-        "a.(b.c)*"  
+        "a.(b.c)*",  
+        "(a|b.c",  
+        "a|b)#c",  
+        "a||b.c"  
     ]
 
     for expr in test_expressions:
-        postfix_expr = shunting_yard(expr)  
-        formatted_postfix = " ".join([s.name for s in postfix_expr])  
-        print(f"Infix: {expr}")
-        print(f"Postfix: {formatted_postfix}\n")
+        try:
+            postfix_expr = shunting_yard(expr)  
+            formatted_postfix = " ".join([s.name for s in postfix_expr])  
+            print(f"Infix: {expr}")
+            print(f"Postfix: {formatted_postfix}\n")
+        except ValueError as e:
+            print(f"Infix: {expr}")
+            print(f"❌ {e}\n")
